822091897b13842aab8ca8d42316547e7f3acba4
ccurielrodrigo@gmail.com <ccurielrodrigo@gmail.com>
2024-01-05 01:06:17 +0900

    Motors are moving on demand!

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
 # Imports
 from flask import Flask, render_template, request
 import RPi.GPIO as GPIO
+from time import sleep
 
 # Initialize GPIO pins
+DELAY_BETWEEN_STEPS = 0.0005  # Adjust this value for speed control
+CYCLES = 500
+
+# Direction pin from controller
+DIR = 10
+# Step pin from controller
+STEP = 8
+# 0/1 used to signify clockwise or counterclockwise.
+CW = 1
+CCW = 0
+
+# Setup pin layout on PI
 GPIO.setmode(GPIO.BOARD)
-DIR_PIN = 10  # Replace with your actual GPIO pin numbers
-STEP_PIN = 8  # Replace with your actual GPIO pin numbers
-GPIO.setup(DIR_PIN, GPIO.OUT)
-GPIO.setup(STEP_PIN, GPIO.OUT)
+
+# Establish Pins in software
+GPIO.setup(DIR, GPIO.OUT)
+GPIO.setup(STEP, GPIO.OUT)
+
+# Set the first direction you want it to spin
+GPIO.output(DIR, CW)
 
 # Instance the app
 app = Flask(__name__)
@@ -20,10 +36,24 @@ def toggle_motor():
     global motor_status
     motor_status = not motor_status
     if motor_status:
-        # Code to start the motor here
+        # Code to start the motor
+        GPIO.output(DIR, CW)
+
+        # Run for 200 steps at a speed determined by DELAY_BETWEEN_STEPS
+        for x in range(CYCLES):
+            GPIO.output(STEP, GPIO.HIGH)
+            sleep(DELAY_BETWEEN_STEPS)
+            GPIO.output(STEP, GPIO.LOW)
+            sleep(DELAY_BETWEEN_STEPS)
         pass
     else:
-        # Code to stop the motor here
+    	# Code to stop the motor here
+        GPIO.output(DIR, CCW)
+        for x in range(CYCLES):
+            GPIO.output(STEP, GPIO.HIGH)
+            sleep(DELAY_BETWEEN_STEPS)
+            GPIO.output(STEP, GPIO.LOW)
+            sleep(DELAY_BETWEEN_STEPS)
         pass
 
 # Endpoints
