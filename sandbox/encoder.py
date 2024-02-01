a8db06786880ffff25b6fba2cf07eea321e94317
ccurielrodrigo@gmail.com <ccurielrodrigo@gmail.com>
2024-02-01 22:30:54 +0900

    Less wait

--- sandbox/encoder.py
+++ sandbox/encoder.py
@@ -38,7 +38,7 @@ def main():
             A, B = read_encoder()
             update_position(A, B)
             print(f"Position: {counter}")
-            sleep(1)
+            sleep(.001)
     except KeyboardInterrupt:
         GPIO.cleanup()
         print("Program exited cleanly")
