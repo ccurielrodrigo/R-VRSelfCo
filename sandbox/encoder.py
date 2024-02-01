9f936822e93cbe2cc8ffe7d6abe155fad2225a7e
ccurielrodrigo@gmail.com <ccurielrodrigo@gmail.com>
2024-02-01 22:29:29 +0900

    Other version of the code for encoders

--- sandbox/encoder.py
+++ sandbox/encoder.py
@@ -1,52 +1,47 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
-counter = 10
+# GPIO pins
+Enc_A = 17
+Enc_B = 27
 
-Enc_A = 17  
-Enc_B = 27  
+# Position counter
+counter = 0
+
+# Encoder state
+last_A = 0
+last_B = 0
 
 def init():
-    print("Rotary Encoder Test Program")
-    GPIO.setwarnings(True)
+    GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(Enc_A, GPIO.IN)
-    GPIO.setup(Enc_B, GPIO.IN)
-    GPIO.add_event_detect(Enc_A, GPIO.RISING, callback=rotation_decode, bouncetime=10)
-    return
+    GPIO.setup(Enc_A, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(Enc_B, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-def rotation_decode(Enc_A):
-    global counter
-    sleep(0.002)
-    Switch_A = GPIO.input(Enc_A)
-    Switch_B = GPIO.input(Enc_B)
+def read_encoder():
+    return GPIO.input(Enc_A), GPIO.input(Enc_B)
 
-    if (Switch_A == 1) and (Switch_B == 0):
-        counter += 1
-        print("direction -> ", counter)
-        while Switch_B == 0:
-            Switch_B = GPIO.input(Enc_B)
-        while Switch_B == 1:
-            Switch_B = GPIO.input(Enc_B)
-        return
-
-    elif (Switch_A == 1) and (Switch_B == 1):
-        counter -= 1
-        print("direction <- ", counter)
-        while Switch_A == 1:
-            Switch_A = GPIO.input(Enc_A)
-        return
-    else:
-        return
+def update_position(A, B):
+    global counter, last_A, last_B
+    if A != last_A or B != last_B:  # Only update on change
+        if A == 1 and B == 0 and last_A == 0:  # Clockwise
+            counter += 1
+        elif A == 0 and B == 1 and last_B == 0:  # Counter-clockwise
+            counter -= 1
+        last_A, last_B = A, B
 
 def main():
+    init()
+    print("Monitoring rotary encoder...")
     try:
-        init()
         while True:
+            A, B = read_encoder()
+            update_position(A, B)
+            print(f"Position: {counter}")
             sleep(1)
-
     except KeyboardInterrupt:
         GPIO.cleanup()
+        print("Program exited cleanly")
 
 if __name__ == '__main__':
     main()
