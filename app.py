882b2e060f2a67d32515f775216bf2dcc5cab3e4
ccurielrodrigo@gmail.com <ccurielrodrigo@gmail.com>
2024-02-04 20:05:07 +0900

    Setup ready. Test pending

--- app.py
+++ app.py
@@ -64,14 +64,12 @@ ENCODERS = {
     "PALM" : {
         "LAST_A" : 0,
         "LAST_B" : 0,
-        "MIN_POSITION" : 0,
         "MAX_POSITION" : 0,
         "CURRENT_POSITION" : 0
     },
     "DORSO" : {
         "LAST_A" : 0,
         "LAST_B" : 0,
-        "MIN_POSITION" : 0,
         "MAX_POSITION" : 0,
         "CURRENT_POSITION" : 0
     },
@@ -118,6 +116,46 @@ def tune_post():
 
     # Return a success response
     return {"status": status}, 200
+
+# Increase or decrease N steps from the exotendon
+@app.route('/setup', methods=['POST'])
+def setup():
+    # Parse the request's body
+    data = request.get_json()
+
+    # Obtain the number of steps and direction
+    setup_type = data.get('setup_type')
+
+    # Validate the information obtained
+    if setup_type is None:
+        return {"error": "Missing 'setup_type'"}, 400
+
+    # Perform the actual step increase
+    status = setup_limit(setup_type)
+
+    # Return a success response
+    return {"status": status}, 200
+
+# Set the exotendon to a limit position (opened or closed)
+@app.route('/control', methods=['POST'])
+def control():
+    # Parse the request's body
+    data = request.get_json()
+
+    # Obtain the number of steps and direction
+    command = data.get('command')
+
+    # Validate the information obtained
+    if command is None:
+        return {"error": "Missing 'command'"}, 400
+
+    # Perform the actual step increase
+    status = setup_control(command)
+
+    # Return a success response
+    return {"status": status}, 200
+
+
     
 '''
 +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
@@ -158,14 +196,10 @@ def tune_post(steps : int, speed: float, direction : str, direction_override : s
             GPIO.output(GPIO_PINS['PALM']['STEP'], GPIO.LOW)
         if (direction_override == "NORMAL" or direction_override == "CLOSE_ONLY"):
             GPIO.output(GPIO_PINS['DORSO']['STEP'], GPIO.LOW)
-        sleep(0.001)
+        sleep(DEBOUNCE_TIME)
 
         # Update it on every rotation
         update_position()
-
-    # TODO: Remove
-    print(ENCODERS['PALM']['CURRENT_POSITION'])
-    print(ENCODERS['DORSO']['CURRENT_POSITION'])
 
     # Stop the process and return success
     process_happening = False
@@ -198,6 +232,93 @@ def update_position():
 
         # Store current state
         ENCODERS['DORSO']['LAST_A'], ENCODERS['DORSO']['LAST_B'] = dorso_a_current, dorso_b_current == 1
+
+# Function for defining the closed or open position of the exotendon
+def setup_limit (setup_type : str):
+    # Check if there is any process happening
+    global process_happening
+    if (process_happening):
+        return False
+
+    # No process happening now. Rise it to avoid mechanical override
+    process_happening = True
+
+    # Set each motor's direction
+    if setup_type == "SET_OPEN":
+        # Indicate this position as the lowest end (set to 0)
+        ENCODERS['DORSO']['CURRENT_POSITION'] = 0
+        ENCODERS['PALM']['CURRENT_POSITION'] = 0
+
+        # Print a signal of success
+        print ("Both encoder's current position successfull reset")
+    else:
+        # Indicate this position as the highest possible
+        ENCODERS['DORSO']['MAX_POSITION'] = ENCODERS['DORSO']['CURRENT_POSITION']
+        ENCODERS['PALM']['MAX_POSITION'] = ENCODERS['PALM']['CURRENT_POSITION']
+        
+        # Print a signal of success
+        print(f'Palm encoder max set as {ENCODERS['PALM']['MAX_POSITION']}')
+        print(f'Dorso encoder max set as {ENCODERS['DORSO']['MAX_POSITION']}')
+
+
+    # Stop the process and return success
+    process_happening = False
+    return True
+
+# Produce a given position on the exotendon
+def setup_control( command : str ): 
+    # Check if there is any process happening
+    global process_happening
+    if (process_happening):
+        return False
+
+    # No process happening now. Rise it to avoid mechanical override
+    process_happening = True
+
+    # Set each motor's direction
+    if command == "OPEN":
+        GPIO.output(GPIO_PINS['PALM']['DIR'], 1)
+        GPIO.output(GPIO_PINS['DORSO']['DIR'], 0)
+    else:
+        GPIO.output(GPIO_PINS['PALM']['DIR'], 0)
+        GPIO.output(GPIO_PINS['DORSO']['DIR'], 1)
+
+    # Perform as much steps as required untill the condition is met
+    global_condition_met = False
+    palm_condition_met = False
+    dorso_condition_met = False
+
+    while not condition_met:
+        # Check for individual requirements of the dorso and palm sensors
+        if (ENCODERS['DORSO']['CURRENT_POSITION'] >= ENCODERS['DORSO']['MAX_POSITION'] or ENCODERS['DORSO']['CURRENT_POSITION'] <= 0):
+            dorso_condition_met = True
+        if (ENCODERS['PALM']['CURRENT_POSITION'] >= ENCODERS['PALM']['MAX_POSITION'] or ENCODERS['PALM']['CURRENT_POSITION'] <= 0):
+            palm_condition_met = True
+
+        # HIGH step
+        if (not palm_condition_met):
+            GPIO.output(GPIO_PINS['PALM']['STEP'], GPIO.HIGH)
+        if (not dorso_condition_met):
+            GPIO.output(GPIO_PINS['DORSO']['STEP'], GPIO.HIGH)
+        sleep(speed)
+
+        # LOW step
+        if (not palm_condition_met):
+            GPIO.output(GPIO_PINS['PALM']['STEP'], GPIO.LOW)
+        if (not dorso_condition_met):
+            GPIO.output(GPIO_PINS['DORSO']['STEP'], GPIO.LOW)
+        sleep(DEBOUNCE_TIME)
+
+        # Update it on every rotation
+        update_position()
+
+        # Check if the condition has been met
+        if (dorso_condition_met and palm_condition_met):
+            global_condition_met = True
+
+    # Stop the process and return success
+    process_happening = False
+    return True
 
 '''
 +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
